728f7fbab214226fc0b540b35a885c69219741d4
ZackInfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2022-05-21 14:36:51 +0200

    Pahe components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
-function App() {
-  return <h2>React Router 6 Tutorial</h2>;
-}
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
+
+/**
+ * you can wrapp all the app with BrowserRouter in index
+ */

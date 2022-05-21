8541d9af7b4c265ff096d7d0ee8fbc0bf258f5ff
ZackInfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2022-05-21 14:43:42 +0200

    Link component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,18 @@
+import { Link } from 'react-router-dom';
+
 const Home = () => {
   return (
     <section className="section">
       <h2>Home Page</h2>
+      <Link to="/about" className="btn">
+        About
+      </Link>
     </section>
   );
 };
 export default Home;
+
+/**
+ * if you want to navigate your project internally ,you would use LINK component, to='/about'
+ * if you want to navigate externally you d rather use <a href="www.google.com">Google</a>
+ */

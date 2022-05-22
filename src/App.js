990bb87416bd1c7f59ab10c592a2d23bdae44629
ZackInfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2022-05-22 14:57:23 +0200

    useNavigate()

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -6,8 +6,12 @@ import Products from './pages/Products';
 import Error from './pages/Error';
 import SharedLayout from './pages/SharedLayout';
 import SingleProduct from './pages/SingleProduct';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
 
 const App = () => {
+  const [user, setUser] = useState(null);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -16,6 +20,8 @@ const App = () => {
           <Route path="about" element={<About />} />
           <Route path="products" element={<Products />} />
           <Route path="products/:productId" element={<SingleProduct />} />
+          <Route path="login" element={<Login setUser={setUser} />} />
+          <Route path="dashboard" element={<Dashboard user={user} />} />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>

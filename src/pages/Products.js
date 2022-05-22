2b0c93ed5854f926dd9c800514796acbee082efe
ZackInfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2022-05-22 14:43:05 +0200

    Single product page

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import products from '../data';
 
 const Products = () => {
   return (
-    <React.Fragment>
-      <section className="section">
-        <h2>products</h2>
-      </section>
-    </React.Fragment>
+    <section className="section">
+      <h2>products</h2>
+      <div className="products">
+        {products.map((product) => {
+          const { id, name } = product;
+          return (
+            <article key={id}>
+              <h5>{name} </h5>
+              <Link to={`/products/${id}`}>more info</Link>
+            </article>
+          );
+        })}
+      </div>
+    </section>
   );
 };
 
